32e1a16b7f4ee9c2ddb13a1626be58593b011315
ylv <ylv@ylvs-MacBook-Pro.local>
2025-01-21 11:51:14 +0700

    add for deploy

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -12,6 +13,8 @@ import { app, server } from "./socket/socket.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 5001;
+
+const __dirname = path.resolve();
 // const app =express();
 
 app.use(express.json());
@@ -20,6 +23,11 @@ app.use(cookieParser());
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/users', userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
 
 // app.listen(5001, () => {
 //     connectToMongoDB();
